75921295b6469ecba03427c257c7cc8caa1edb0a
kkyouma <144501527+kkyouma@users.noreply.github.com>
2023-10-30 05:00:29 -0300

    add hover event to draw

--- main.js
+++ main.js
@@ -2,7 +2,21 @@ const mainContainer = document.getElementById('mainContainer');
 
 for(i=1; i <= 16*16; i++) {
   const div = document.createElement('div');
-  div.setAttribute('class', 'element');
+  div.setAttribute('class', '');
   mainContainer.appendChild(div);
-  div.textContent = `${i}`;
 }
+
+const pixelDraw = document.querySelectorAll('.container > div');
+
+pixelDraw.forEach(function(element){
+  element.addEventListener('mouseover', function() {
+    element.classList.add('black');
+  });
+});
+
+pixelDraw.forEach(function(element){
+  element.addEventListener('mouseout', function() {
+    element.classList.remove('black');
+  });
+});
+
