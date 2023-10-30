78138ed8339df9679b4e13d82986de07d280a051
kkyouma <144501527+kkyouma@users.noreply.github.com>
2023-10-30 02:47:26 -0300

    add text div in the DOM

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const mainContainer = document.getElementById('mainContainer');
 
-for(i=0; i < 16; ++i) {
+for(i=1; i <= 16; ++i) {
   const div = document.createElement('div');
   div.setAttribute('class', 'element');
+  mainContainer.appendChild(div);
   div.textContent = `${i}`;
   console.log(div);
 }
