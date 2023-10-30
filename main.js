a67b508c1b4b6c91b9f505955efc7336f942a459
kkyouma <144501527+kkyouma@users.noreply.github.com>
2023-10-30 03:25:23 -0300

    add centred container

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 const mainContainer = document.getElementById('mainContainer');
 
-for(i=1; i <= 16; ++i) {
+for(i=1; i <= 16*16; i++) {
   const div = document.createElement('div');
   div.setAttribute('class', 'element');
   mainContainer.appendChild(div);
   div.textContent = `${i}`;
-  console.log(div);
 }
-console.log(i)
